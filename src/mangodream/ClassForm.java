0365f667feef10ff1434ff7a4463c9546dba4159
Vladimir <data-box@mail.com>
2013-04-18 02:48:11 +0400

    18.04.13 - ended with textfield and button

--- src/mangodream/ClassForm.java
+++ src/mangodream/ClassForm.java
@@ -1,18 +1,32 @@
 package mangodream;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class ClassForm  {
 
     private JFrame MainFormView = new JFrame("Testing Windows");
+    private JTextField TaskFieldView = new JTextField();
       
-public void createFormWithComponents() {
+public void createFormWithComponents()  {
     MainFormView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    MainFormView.setSize(800, 600);   
+    MainFormView.setSize(500, 400);   
     MainFormView.setVisible(true);
     MainFormView.setResizable(false);
     MainFormView.setLayout(null);
     MainFormView.setLocationRelativeTo(null);
+    textfieldCreate();
+    MainFormView.add(TaskFieldView);
 }
+
+public void textfieldCreate()  {
+    TaskFieldView.setHorizontalAlignment(JTextField.RIGHT);
+    TaskFieldView.setBounds(5, 10, 360, 20);
+    TaskFieldView.setVisible(true);   
+} 
+
+public void buttonPostTaskCreate()  {
     
+}
+
 }
