0dfed674bf46cfcf754512bd2fffa68778f06e5a
Vladimir <data-box@mail.com>
2013-04-20 14:28:29 +0400

    20.04.13 - add data base connection class. added button for posting tasks

--- src/mangodream/ClassForm.java
+++ src/mangodream/ClassForm.java
@@ -1,32 +1,37 @@
 package mangodream;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class ClassForm  {
 
     private JFrame MainFormView = new JFrame("Testing Windows");
+    private JButton SendTaskButton = new JButton();
     private JTextField TaskFieldView = new JTextField();
       
 public void createFormWithComponents()  {
     MainFormView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    MainFormView.setSize(500, 400);   
+    MainFormView.setSize(480, 400);   
     MainFormView.setVisible(true);
     MainFormView.setResizable(false);
     MainFormView.setLayout(null);
     MainFormView.setLocationRelativeTo(null);
     textfieldCreate();
+    buttonPostTaskCreate();
     MainFormView.add(TaskFieldView);
+    MainFormView.add(SendTaskButton);
 }
 
 public void textfieldCreate()  {
-    TaskFieldView.setHorizontalAlignment(JTextField.RIGHT);
+    TaskFieldView.setHorizontalAlignment(JTextField.LEFT);
     TaskFieldView.setBounds(5, 10, 360, 20);
     TaskFieldView.setVisible(true);   
 } 
 
 public void buttonPostTaskCreate()  {
-    
+    SendTaskButton.setBounds(370, 10, 100, 19);
+    SendTaskButton.setVisible(true);
+    SendTaskButton.setText("Add task");
 }
-
 }
