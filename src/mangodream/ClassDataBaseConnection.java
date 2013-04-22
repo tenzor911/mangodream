eeb3b11120cb3ef9c390917e13c53095d8d0e46c
Vladimir <data-box@mail.com>
2013-04-23 00:42:53 +0400

    21.04.13 - configured connection with database

--- src/mangodream/ClassDataBaseConnection.java
+++ src/mangodream/ClassDataBaseConnection.java
@@ -1,13 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mangodream;
 
-/**
- *
- * @author tenzor
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.apache.derby.jdbc.ClientDriver;
+
 public class ClassDataBaseConnection {
     
+    private String dbHost = "jdbc:derby://localhost:1527/mangodream";
+    private String uName = "root";
+    private String uPass = "root";
+    
+    void DataBaseConnectionInitiating() throws ClassNotFoundException {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection dbConnection = DriverManager.getConnection(dbHost, uName, uPass);
+            System.out.println("Connect to " + dbConnection.getCatalog() + " a success!");
+        }
+        catch (SQLException err) {
+            System.err.println("");
+            System.err.println("Error!!! Connection was unsuccessfull !!!" + err.getMessage());  
+        }
+    }
 }
--- src/mangodream/ClassMain.java
+++ src/mangodream/ClassMain.java
@@ -1,8 +1,10 @@
 package mangodream;
 
 public class ClassMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         ClassForm CallClassForm = new ClassForm();
         CallClassForm.createFormWithComponents();
-    }
+        ClassDataBaseConnection Conn = new ClassDataBaseConnection();
+        Conn.DataBaseConnectionInitiating();
+}
 }
